dbc9d79091fd38f6c68c90baddc386fa6b844bdb
jamestaylr <jamestaylr@gmail.com>
2015-04-02 12:05:20 -0400

    Edit launch script for clarity
    Fixed a directory error in the `launch.sh` script and also removed the
    temporary variable to make the conversion process clearer.

--- launch.sh
+++ launch.sh
@@ -3,15 +3,16 @@
 echo '\033[32m\033[1mPrecompiling the CSS...\033[0m\033[39m'
 
 # Searches the CSS directory for LESS files
-for file in views/css/*.less
+for file in views/less/*.less
 do
     FROM=$file
-    A=${file/.*/.css}
-    TO=${A/views/resources}
+    TO=${file/.*/.css}
+    TO=${TO/less/css}
+    TO=${TO/views/resources}
     echo "$FROM -> $TO"
     # Compiles each LESS file into a CSS file of the same name with minified output
     lessc --clean-css $FROM $TO
 done
 
-echo '\033[32m\033[1mStarting the server!\033[0m\033[39m'
+echo '\033[32m\033[1mStarting Node process!\033[0m\033[39m'
 node server.js
