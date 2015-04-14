9d27c119fff29f06108f7eac88acccb7a003ca9d
jamestaylr <jamestaylr@gmail.com>
2015-04-14 11:27:48 -0400

    Authored generic logging methods
    `getFormattedDate()` formats the current time / date into a form used
    in the generation of the server log files. Logging also writes to a
    file and `stdout`.

--- server.js
+++ server.js
@@ -10,3 +10,17 @@ app.get('/', function(req, res) {
 });
 
 app.listen(8080);
+/*
+ * Returns a fomatted date as a String in brackets, for logging purposes
+ */
+function getFormattedDate() {
+    return "[" + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + "] ";
+}
+
+/*
+ * Overloads the logging function 
+ */ 
+console.log = console.error = function(message) {
+    log_file.write(util.format(message) + '\n');
+    log_stdout.write(util.format(message) + '\n');
+};
