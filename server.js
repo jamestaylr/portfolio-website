e0c9aa962132838ef04566fa5ab3f61862e6cea0
jamestaylr <jamestaylr@gmail.com>
2015-04-18 11:19:39 -0400

    Define 404 error handling functions

--- server.js
+++ server.js
@@ -7,9 +7,36 @@ app.set('view engine', 'ejs');
 // Setup the index page view
 app.get('/', function(req, res) {
 	res.render('pages/index');
+    // Defines error handling
+    app.use(function(req, res, next) {
+        res.status(404);
+
+        // Respond with HTML page
+        if (req.accepts('html')) {
+            res.render('404', {
+                url: req.url
+            });
+            return;
+        }
+
+        // Respond with JSON
+        if (req.accepts('json')) {
+            res.send({
+                error: 'Not found'
+            });
+            return;
+        }
+
+        // Default to plain-text
+        res.type('txt').send('Not found');
+    });
+
+    // Bind the server process on the port
+    var server = app.listen(port, function() {
+        console.log("Server successfully started!");
+    });
 });
 
-app.listen(8080);
 /*
  * Returns a fomatted date as a String in brackets, for logging purposes
  */
